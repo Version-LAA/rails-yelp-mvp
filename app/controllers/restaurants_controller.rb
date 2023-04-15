057653eb4254fa0670070326f1a1689facb8e3b7
Latoya Alford <lat.jamison@gmail.com>
2023-04-15 15:01:25 -0400

    created a display for all restaurants and a single restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,9 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaraunt = Restaurant.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/restaurants", to: "restaurants#index"
+  get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
 end
