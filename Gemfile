219ba43091fe55e68231fdc0bae5ac6d2c906368
Latoya Alford <lat.jamison@gmail.com>
2023-04-15 14:13:52 -0400

    created seed data for database

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "jbuilder"
 # simple form
 gem "simple_form"
 
+# faker for seed data
+gem "faker"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Initiating seed data'
+Restaurant.destroy_all
+puts 'cleaned database'
+15.times do
+  restaraunt = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "created restaurant #{restaraunt.id}"
+end
+puts 'restaurants created successfully'
