ecad82d300e2de97923dd8952a728d932899e7b2
Latoya Alford <lat.jamison@gmail.com>
2023-04-15 14:02:50 -0400

    created models and implemented validators

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_154139) do
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
