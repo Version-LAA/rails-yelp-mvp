60285da244aee4d2edaf588678beed64fc096bc7
Latoya Alford <lat.jamison@gmail.com>
2023-04-15 15:59:48 -0400

    implemented create new restaurant feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,4 +10,17 @@ class RestaurantsController < ApplicationController
   def new
     @restaraunt = Restaurant.new
   end
+
+  def create
+    @restaraunt = Restaurant.new(restaraunt_params)
+    @restaraunt.save
+    redirect_to restaurant_path(@restaraunt)
+  end
+
+  private
+
+  def restaraunt_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/restaurants", to: "restaurants#index"
   get "/restaurants/new", to: "restaurants#new"
   get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
+  post "/restaurants", to: "restaurants#create"
 end
