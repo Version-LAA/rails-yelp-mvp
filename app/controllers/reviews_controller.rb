9a455670679811b8a5153db3869b0b5c052f0c56
Latoya Alford <lat.jamison@gmail.com>
2023-04-16 10:56:07 -0400

    moved review form to show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,19 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
+  def create
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new(restaurant_params)
+
+    @review.restaurant = @restaurant
+
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:review).permit(:rating, :content)
   end
 end
+
+# review
+# reviews
+# Review
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # post "/restaurants", to: "restaurants#create"
 
   resources :restaurants do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:create]
   end
 
 
